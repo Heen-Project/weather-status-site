da71302dd469a992d5197b00a62fd5e37734de7a
Wendy <61735806+Heen-Project@users.noreply.github.com>
2020-06-01 22:47:07 +0700

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('postman-request');
 
-//// https://www.mapbox.com/ //username:noel23 email:noel@superrito.com pass:noelnoel
+//// https://www.mapbox.com/
 const geocode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/'+ encodeURIComponent(address) +'.json?access_token=pk.eyJ1Ijoibm9lbDIzIiwiYSI6ImNrYXZ6cjU4eDFsNWMyeXAzOWhraGtieXAifQ.ZgNJR4zn7wIh4I0me4OZWA&limit=1';
     request({url, json:true}, (error, {body} = {}) => {
